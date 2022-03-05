e194654023ddee9b90ae0090cf531976fc642ba4
michaelrinos <mrinos98@gmail.com>
2022-03-05 17:42:04 -0500

    User

---
+++ Managers/UserManager.cs
@@ -0,0 +1,16 @@
+﻿using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Managers {
+    public static class UserManager {
+        /*
+        public static User Create(User user)
+        {
+
+        }
+        */
+    }
+}
